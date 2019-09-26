5cbacd0e7c7505ff7152a56cb380bd5f75b845c7
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:32:28 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan4-Kambing/src/pbo1/pkg10118002/latihan4/kambing/PBO110118002Latihan4Kambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan4.kambing;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ */
+public class PBO110118002Latihan4Kambing {
+public void tambahKambing() {
+//Deklarasi variabel lokal
+    int jumlahKambing = 0;
+    
+    jumlahKambing = jumlahKambing + 5;
+    System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    PBO110118002Latihan4Kambing kambingJantan = new PBO110118002Latihan4Kambing();
+    kambingJantan.tambahKambing();
+    }
+    
+}
